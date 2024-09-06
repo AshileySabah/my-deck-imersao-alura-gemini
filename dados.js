22a5eaa1ddb0fcedf3d0910f0664fc61d29cad43
AshileySabah <ashileysabrina2002@gmail.com>
2024-09-05 22:29:13 -0300

    feat: dados

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+  {
+    tipo: "Terreno",
+    nome: "Montanha Fumegante",
+    descricao:
+      "Um vulcão ativo que cospe lava incandescente, oferecendo mana vermelha e um campo de batalha infernal.",
+    dano: 0,
+    defesa: 0,
+  },
+  {
+    tipo: "Criatura",
+    nome: "Fênix Imortal",
+    descricao:
+      "Uma criatura mítica que renasce das cinzas, mais forte a cada vez que é derrotada. Suas chamas purificadoras consomem seus inimigos.",
+    dano: 10,
+    defesa: 1000,
+  },
+  {
+    tipo: "Magia",
+    nome: "Tempestade Elétrica",
+    descricao:
+      "Raios racham o céu, atingindo todos os inimigos em campo com um estrondo ensurdecedor, causando dano e paralisando-os por um breve momento.",
+    dano: 10,
+    defesa: 0,
+  },
+  {
+    tipo: "Artefato",
+    nome: "Escudo da Fé",
+    descricao:
+      "Forjado em um tempo antigo, este escudo mágico protege seu portador de qualquer mal, dissipando feitiços e repelindo ataques.",
+    dano: 0,
+    defesa: 4,
+  },
+];
